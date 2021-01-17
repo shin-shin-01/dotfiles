af77942224898801623ff8b2c4141115b9a86fcf
Kaze Shindo <kaze.bknb@outlook.jp>
2021-01-18 00:06:43 +0900

    add vscode setting

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -snf ~/work/dotfiles/.zshrc ~/.zshrc
 ln -snf ~/work/dotfiles/.zprofile ~/.zprofile
 ln -snf ~/work/dotfiles/enhanced ~/enhanced
 ln -snf ~/work/dotfiles/.gitconfig ~/.gitconfig
+ln -snf ~/Library/Application\ Support/Code/User/keybindings.json ./vscode/keybindings.json
+ln -snf ~/Library/Application\ Support/Code/User/settings.json ./vscode/settings.json
+
