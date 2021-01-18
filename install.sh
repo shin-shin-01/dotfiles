618f0f9805817b16b86f7fc10c43074a117df19a
Kaze Shindo <kaze.bknb@outlook.jp>
2021-01-18 17:44:05 +0900

    fix command

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ ln -snf ~/work/dotfiles/.zshrc ~/.zshrc
 ln -snf ~/work/dotfiles/.zprofile ~/.zprofile
 ln -snf ~/work/dotfiles/enhanced ~/enhanced
 ln -snf ~/work/dotfiles/.gitconfig ~/.gitconfig
-ln -snf ~/Library/Application\ Support/Code/User/keybindings.json ./vscode/keybindings.json
-ln -snf ~/Library/Application\ Support/Code/User/settings.json ./vscode/settings.json
+ln -snf ~/work/dotfiles/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -snf ~/work/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
